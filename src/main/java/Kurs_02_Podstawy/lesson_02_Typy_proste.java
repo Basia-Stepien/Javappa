b09085e74df9c6ba9635a06ebe2651f248acd851
Basia-Stepien <bases_01@protonmail.com>
2022-08-25 08:25:15 +0200

    lesson_02_Typy_proste

---
+++ src/main/java/Kurs_02_Podstawy/lesson_02_Typy_proste.java
@@ -0,0 +1,31 @@
+package Kurs_02_Podstawy;
+
+public class lesson_02_Typy_proste {
+    public static void main(String[] args) {
+        System.out.println("Hello JavAPPa");
+
+        char itemFirstCharacter = 'i';
+        System.out.println(itemFirstCharacter);
+
+        boolean enabled = true;
+        System.out.println(enabled);
+
+        byte itemsNumberByte = 7;
+        System.out.println(itemsNumberByte);
+
+        short itemsNumberShort = 400;
+        System.out.println(itemsNumberShort);
+
+        int itemsNumberInt = 53000;
+        System.out.println(itemsNumberInt);
+
+        long itemsNumberLong = 72036854775807L;
+        System.out.println(itemsNumberLong);
+
+        float itemsNumberFloat = 4.33f;
+        System.out.println(itemsNumberFloat);
+
+        double itemsNumberDouble = 4.99;
+        System.out.println(itemsNumberDouble);
+    }
+}
