13bda41017931012172b35a6f54406b88f967465
Basia-Stepien <bases_01@protonmail.com>
2022-09-02 09:55:18 +0200

    lesson_07_Petla_for

---
+++ src/main/java/Kurs_02_Podstawy/lesson_07_Petla_for.java
@@ -0,0 +1,19 @@
+package Kurs_02_Podstawy;
+
+public class lesson_07_Petla_for {
+    public static void main(String[] args) {
+        System.out.println("Hello JavAPPa");
+
+        System.out.println("Inkrementacja:");
+
+        for (int itemNumber = 0; itemNumber < 5; itemNumber++) {
+            System.out.println(itemNumber);
+        }
+
+        System.out.println("Dekrementacja:");
+
+        for (int itemNumberMax = 8; itemNumberMax > 0; itemNumberMax--) {
+            System.out.println(itemNumberMax);
+        }
+    }
+}
