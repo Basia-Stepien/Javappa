12f19c6d8866d524b39feb6e486082d84a7592de
Basia-Stepien <bases_01@protonmail.com>
2022-09-14 14:29:01 +0200

    Kurs_03ProgramowanieObiektowe.lesson03WlasneObiekty

---
+++ src/main/java/Kurs_03ProgramowanieObiektowe/lesson03WlasneObiekty/Category.java
@@ -0,0 +1,28 @@
+package Kurs_03ProgramowanieObiektowe.lesson03WlasneObiekty;
+
+public class Category {
+
+    int id;
+    String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/Kurs_03ProgramowanieObiektowe/lesson03WlasneObiekty/Start.java
@@ -0,0 +1,10 @@
+package Kurs_03ProgramowanieObiektowe.lesson03WlasneObiekty;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello JavAPPa");
+        Item item = new Item(5, "Appa Item 1", new Category(1, "Appa Category 1"));
+        System.out.println(item.getName());
+        System.out.println(item.getCategory().getName());
+    }
+}
