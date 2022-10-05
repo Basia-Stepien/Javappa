3350d9dcd72afdd143e555ea259c706816385dd9
Basia-Stepien <bases_01@protonmail.com>
2022-10-05 12:26:30 +0200

    Kurs_03ProgramowanieObiektowe.lesson11_21.lesson12ModyfikatoryDostepuINiedostepowe

---
+++ src/main/java/Kurs_03ProgramowanieObiektowe/lesson11_21/lesson12ModyfikatoryDostepuINiedostepowe/Item.java
@@ -0,0 +1,13 @@
+package Kurs_03ProgramowanieObiektowe.lesson11_21.lesson12ModyfikatoryDostepuINiedostepowe;
+
+public class Item {
+    int id;
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    int getId() {
+        return id;
+    }
+}
---
+++ src/main/java/Kurs_03ProgramowanieObiektowe/lesson11_21/lesson12ModyfikatoryDostepuINiedostepowe/Start.java
@@ -0,0 +1,8 @@
+package Kurs_03ProgramowanieObiektowe.lesson11_21.lesson12ModyfikatoryDostepuINiedostepowe;
+
+public class Start {
+    public static void main(String[] args) {
+        Item item = new Item(1);
+        System.out.println(item.getId());
+    }
+}
